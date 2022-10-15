4a67ce37bff0e737a1773cabdfb57c9d1297872a
Avery Uslaner <uslaner.avery@gmail.com>
2022-10-15 16:52:21 -0600

    Refactor relay appliances into their own functions.
    Paludarium gets more advanced logic to turn things off after a period of time

--- main.py
+++ main.py
@@ -30,16 +30,98 @@ def switch_pin(output: DigitalOutputDevice):
         output.on()
 
 
+def control_fan(state: str = 'OFF'):
+    """
+    Change the state of the cooling fan.
+    Note that the relay's off method actually turns on the appliance connected it
+    and vice versa.
+    """
+    if state.upper() == 'ON':
+        logging.info('Turning ON fan connected to relay one...')
+        relay_one.off()
+    elif state.upper() == 'OFF':
+        logging.info('Turning OFF fan connected to relay one...')
+        relay_one.on()
+    else:
+        return Exception('state should be given as ON or OFF')
+
+
+def control_fog(state: str = 'OFF'):
+    """
+    Control the mister.
+    Note that the relay's off method actually turns on the appliance connected it
+    and vice versa.
+    """
+    if state.upper() == 'ON':
+        logging.info('Turning ON mister/fogger connected to relay two...')
+        relay_two.off()
+    elif state.upper() == 'OFF':
+        logging.info('Turning OFF mister/fogger connected to relay two...')
+        relay_two.on()
+    else:
+        return Exception('state should be given as ON or OFF')
+
+
+def control_light(state: str = 'OFF'):
+    """
+    Control the main paludarium light.
+    Note that the relay's off method actually turns on the appliance connected it
+    and vice versa.
+    """
+    if state.upper() == 'ON':
+        logging.info('Turning ON main light connected to relay three...')
+        relay_three.off()
+    elif state.upper() == 'OFF':
+        logging.info('Turning OFF main light connected to relay three...')
+        relay_three.on()
+    else:
+        return Exception('state should be given as ON or OFF')
+
+
+def control_water(state: str = 'OFF'):
+    """
+    Control the water pump.
+    Note that the relay's off method actually turns on the appliance connected it
+    and vice versa.
+    """
+    if state.upper() == 'ON':
+        logging.info('Turning ON water pump connected to relay four...')
+        relay_four.off()
+    elif state.upper() == 'OFF':
+        logging.info('Turning OFF water pump connected to relay four...')
+        relay_four.on()
+    else:
+        return Exception('state should be given as ON or OFF')
+
+
+def cool_down():
+    """
+    Try to cool down the paludarium
+    """
+    logging.info('Turning fan on to cool down tank...')
+    control_fan('ON')
+    time.sleep(1)
+    logging.info('Turning light off to help cool down tank...')
+    control_light('OFF')
+    time.sleep(1)
+    logging.info('Turning water pump on so fogger has water in the top reservoir...')
+    control_water('ON')
+    water_timer = threading.Timer(10.0, control_water, ['OFF'])
+    logging.info('Starting timer to turn off water in 10 seconds...')
+    water_timer.start()
+    logging.info('Turning fogger on...')
+    control_fog('ON')
+    fog_timer = threading.Timer(270.0, control_fog, ['OFF'])
+    logging.info('Starting timer to turn fogger off in 270 seconds...')
+    fog_timer.start()
+
+
 def handle_fan_signal():
     logging.info('Handling fan signals...')
     while True:
         logging.debug('Checking fan signal pin...')
         if fan_signal.value == 1:  # Signal that fan should be on
-            logging.info('Turning ON relay one...')
-            relay_one.off()  # Off actually turns it on because of how it is wired in the relay
-            time.sleep(1)
-            logging.info('Turning OFF relay three...')
-            relay_three.on()  # Turn off the light to reduce heat
+            cool_down()
         else:
             logging.info('Turning OFF relay one...')
             relay_one.on()
